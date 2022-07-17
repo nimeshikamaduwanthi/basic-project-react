a0762bb692369b050d3b7f22eaf6f0ab45833751
nimeshikagit <nimemobile@gmail.com>
2022-07-17 13:15:02 +0530

    sidebar-modal-commit

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,20 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  const { openSidebar, openModal } = useGlobalContext();
+  console.log();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show model
+      </button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
