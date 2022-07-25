aa30afb8b972b18e3c69cb5788921b23e9e34f53
nimeshikagit <nimemobile@gmail.com>
2022-07-25 09:52:00 +0530

    cocktails-app

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor natus ab
+        illum totam iure, expedita beatae corrupti aperiam consequatur quibusdam
+        perspiciatis quasi eos praesentium, asperiores nostrum harum laudantium
+        a vero labore vitae reprehenderit quam debitis saepe voluptates. Iste
+        ipsam autem tenetur perspiciatis a ex natus vel aliquam velit commodi?
+        Ab!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
